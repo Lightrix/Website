b303d658638223bcb1fd2e104973777932e37a13
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-11 13:47:56 +0200

    "Update astro.config.ts: Replace sitemap/prefetch with critters/rome"

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,11 @@
 // @ts-nocheck
 import { defineConfig } from "astro/config";
 
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import critters from "astro-critters";
-import sitemap from "@astrojs/sitemap";
 import rome from "astro-rome";
-import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
 	integrations: [
