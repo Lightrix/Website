0817fc547cd6aeea7c4b266ac15e6bababb4ce77
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-21 23:25:02 +0200

    squash!

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-	apiKey: "", // TODO Replace with your API key
-	appId: "", // TODO Replace with your app ID
-	authDomain: "", // TODO Replace with your auth domain
-	databaseURL: "", // TODO Replace with your database URL
-	measurementId: "", // TODO Replace with your measurement ID
-	messagingSenderId: "", // TODO Replace with your messaging sender ID
-	projectId: "", // TODO Replace with your project ID
-	storageBucket: "", // TODO Replace with your storage bucket
+	apiKey: "AIzaSyBnzUXdWaTxrKzC2re22igcizeRn0k5-nQ",
+	appId: "1:575707899752:web:d92a6be26711a8365218dd",
+	authDomain: "lightrix-web.firebaseapp.com",
+	measurementId: "G-KN9GFNTD0G",
+	messagingSenderId: "575707899752",
+	projectId: "lightrix-web",
+	storageBucket: "lightrix-web.appspot.com",
 };
 
 initializeApp(firebaseConfig);
