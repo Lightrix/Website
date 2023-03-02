1eba0e26d778d6f667ad09e0ba56c4489a23f723
Nikola Hristov <nikola@nikolahristov.tech>
2023-03-02 19:15:19 +0200

    squash!

---
+++ src/scripts/swup.ts
@@ -0,0 +1,21 @@
+// @ts-ignore
+import Swup from "swup";
+
+// @ts-ignore
+import SwupBodyClassPlugin from "@swup/body-class-plugin";
+// @ts-ignore
+import SwupHeadPlugin from "@swup/head-plugin";
+// @ts-ignore
+import SwupPreloadPlugin from "@swup/preload-plugin";
+// @ts-ignore
+import SwupScrollPlugin from "@swup/scroll-plugin";
+
+new Swup({
+	containers: ["#footer", "#main", "#header"],
+	plugins: [
+		new SwupHeadPlugin(),
+		new SwupPreloadPlugin(),
+		new SwupScrollPlugin(),
+		new SwupBodyClassPlugin(),
+	],
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: [
 		"./public/**/*.html",
 		"./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}",
 	],
-	darkMode: "media",
 	theme: {
 		container: {
 			center: true,
 		},
 		extend: {
+			transitionTimingFunction: {
+				apple: "cubic-bezier(0.25, 0.1, 0.25, 1)",
+			},
+			fontFamily: {
+				sans: ["Albert Sans", ...defaultTheme.fontFamily.sans],
+			},
 			typography: {
 				DEFAULT: {
 					css: {
@@ -17,6 +24,12 @@ module.exports = {
 						},
 					},
 				},
+			},
+			colors: {
+				"lightrix": "#FF2323",
+			},
+			ringWidth: {
+				"5": "5px",
 			},
 		},
 	},
