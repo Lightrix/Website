4f48b2de135c793c8eb24d0f486ef9ebd787b812
Nikola Hristov <nikola@nikolahristov.tech>
2023-04-28 13:37:30 +0300

    squash!

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,8 @@ module.exports = {
 			fontFamily: {
 				sans: ["Albert Sans", ...defaultTheme.fontFamily.sans],
 			},
-			typography: {
+			// @ts-ignore
+			typography: ({ theme }) => ({
 				DEFAULT: {
 					css: {
 						a: {
@@ -26,7 +27,61 @@ module.exports = {
 						},
 					},
 				},
-			},
+				lightrix: {
+					css: {
+						"--tw-prose-body": theme("colors.lightrix[700]"),
+						"--tw-prose-headings": theme("colors.lightrix[900]"),
+						"--tw-prose-lead": theme("colors.lightrix[600]"),
+						"--tw-prose-links": theme("colors.lightrix[900]"),
+						"--tw-prose-bold": theme("colors.lightrix[900]"),
+						"--tw-prose-counters": theme("colors.lightrix[500]"),
+						"--tw-prose-bullets": theme("colors.lightrix[300]"),
+						"--tw-prose-hr": theme("colors.lightrix[200]"),
+						"--tw-prose-quotes": theme("colors.lightrix[900]"),
+						"--tw-prose-quote-borders": theme(
+							"colors.lightrix[200]"
+						),
+						"--tw-prose-captions": theme("colors.lightrix[500]"),
+						"--tw-prose-code": theme("colors.lightrix[900]"),
+						"--tw-prose-pre-code": theme("colors.lightrix[200]"),
+						"--tw-prose-pre-bg": theme("colors.lightrix[800]"),
+						"--tw-prose-th-borders": theme("colors.lightrix[300]"),
+						"--tw-prose-td-borders": theme("colors.lightrix[200]"),
+						"--tw-prose-invert-body": theme("colors.lightrix[900]"),
+						"--tw-prose-invert-headings": theme("colors.white"),
+						"--tw-prose-invert-lead": theme("colors.lightrix[400]"),
+						"--tw-prose-invert-links": theme("colors.white"),
+						"--tw-prose-invert-bold": theme("colors.white"),
+						"--tw-prose-invert-counters": theme(
+							"colors.lightrix[400]"
+						),
+						"--tw-prose-invert-bullets": theme(
+							"colors.lightrix[600]"
+						),
+						"--tw-prose-invert-hr": theme("colors.lightrix[700]"),
+						"--tw-prose-invert-quotes": theme(
+							"colors.lightrix[100]"
+						),
+						"--tw-prose-invert-quote-borders": theme(
+							"colors.lightrix[700]"
+						),
+						"--tw-prose-invert-captions": theme(
+							"colors.lightrix[400]"
+						),
+						"--tw-prose-invert-code": theme("colors.white"),
+						"--tw-prose-invert-pre-code": theme(
+							"colors.lightrix[300]"
+						),
+						"--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+						"--tw-prose-invert-th-borders": theme(
+							"colors.lightrix[600]"
+						),
+						"--tw-prose-invert-td-borders": theme(
+							"colors.lightrix[700]"
+						),
+					},
+				},
+			}),
 			colors: {
 				"lightrix": {
 					"50": "#fff1f1",
