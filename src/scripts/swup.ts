adb747b6acef776f8fdc54621fc5717d7221ad79
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-08 16:05:59 +0300

    squash!

--- src/scripts/swup.ts
+++ src/scripts/swup.ts
@@ -4,11 +4,11 @@ import Swup from "swup";
 // @ts-ignore
 import SwupBodyClassPlugin from "@swup/body-class-plugin";
 // @ts-ignore
-import SwupHeadPlugin from "@swup/head-plugin";
-// @ts-ignore
 import SwupPreloadPlugin from "@swup/preload-plugin";
 // @ts-ignore
 import SwupScrollPlugin from "@swup/scroll-plugin";
+// @ts-ignore
+import SwupHeadPlugin from "@swup/head-plugin";
 
 new Swup({
 	containers: ["#footer", "#main", "#header"],
