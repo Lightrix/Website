418eb56cf2067e93bd92b21a60c3cf9c9f344b40
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-26 01:32:37 +0300

    squash!

--- Source/Script/swup.ts
+++ Source/Script/swup.ts
@@ -1,13 +1,14 @@
 import SwupBodyClassPlugin from "@swup/body-class-plugin";
 import SwupHeadPlugin from "@swup/head-plugin";
+import SwupOverlayTheme from "@swup/overlay-theme";
 import SwupPreloadPlugin from "@swup/preload-plugin";
 import SwupScrollPlugin from "@swup/scroll-plugin";
 import Swup from "swup";
 
 new Swup({
-	// @TODO: Put in block-editor
 	containers: ["#footer", "#main", "#header"],
 	plugins: [
+		new SwupOverlayTheme(),
 		new SwupHeadPlugin(),
 		new SwupPreloadPlugin(),
 		new SwupScrollPlugin(),
